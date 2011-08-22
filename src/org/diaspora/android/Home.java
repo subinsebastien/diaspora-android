3e93c9dcfe5981116802e5eec28668c166984766
Napster <napster@godrig>
2011-08-22 16:46:28 +0530

    added menu inflater to Home.java

--- src/org/diaspora/android/Home.java
+++ src/org/diaspora/android/Home.java
@@ -4,6 +4,8 @@ import org.diaspora.android.R;
 import org.diaspora.helpers.DiasporaActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class Home extends DiasporaActivity {
     /** Called when the activity is first created. */
@@ -12,4 +14,15 @@ public class Home extends DiasporaActivity {
         super.onCreate(savedInstanceState);
         setDiasporaContentView(R.layout.home);
     }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		//return super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_menu, menu);
+		return true;
+	}
+    
+    
 }
